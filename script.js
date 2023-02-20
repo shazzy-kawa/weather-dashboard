5cd7229ffebae785ee029ffd467793a68d40b83d
shazzy-kawa <shaz_saleh@live.co.uk>
2023-02-20 19:11:23 +0000

    modified files

--- script.js
+++ script.js
@@ -20,6 +20,8 @@ $('#search-form').on('submit', function(event) {
             const lat = response[0].lat;
             const lon = response[0].lon;
 
+            
+
             const weatherQueryUrl = 'http://api.openweathermap.org/data/2.5/forecast?units=metric&lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
 
             // Call 5 day weather forecast API after we have city lat and lon value
@@ -28,12 +30,13 @@ $('#search-form').on('submit', function(event) {
                     // Put the response on the HTML page
                     const weatherList = weatherResponse.list;
                     console.log(weatherResponse);
+                    showCurrentWeather(weatherList[0], userInput);
 
                     const weathers = [];
                     for (let i = 0; i < weatherList.length; i += 8) {
                         weathers.push(weatherList[i]);
                     }
-
+                    showFiveDayWeather(weathers)
                     // weathers[0] will be today's weather
                     // weathers[1 - 4] will be 5 days forecast
                     // TODO: put today's weather in container for today's weather
@@ -43,6 +46,33 @@ $('#search-form').on('submit', function(event) {
         });
 });
 
+function showCurrentWeather(data, cityName){
+
+    var currentWeatherHTML = `
+    <div class="card">
+        <div class="card-body">
+        <h2>${cityName} (${data.dt_txt}) <img scr="https://openweathermap.org/img/w/${data.weather[0].icon}.png" /> </h2>
+        <p>Temp: ${data.main.temp}</p>
+        <p>Wind: ${data.wind.speed}</p>
+        <p>Humidity: ${data.main.humidity}%</p>
+        </div>
+    </div>
+    `
+
+    $("#today").html(currentWeatherHTML)
+}
+
+function showFiveDayWeather(data){
+    var fiveDayHTML = ``
+
+    for(var i =0; i < data.length; i++){
+        var currentDay = data[i];
+
+        fiveDayHTML += ``
+    }
+
+    $().html(fiveDayHTML)
+}
 //4.Put response in todays weather container for todays weather in HTML page
 
 //5.Put 5 day forecast in container on HTML page
